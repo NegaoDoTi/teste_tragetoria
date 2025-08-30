c8bd81a2b371f7ea75c68f28c7b062478bb27e9b
Vinicius Luciano <viniciusluciano2012@hotmail.com>
2025-08-30 04:31:58 -0300

    feat: criado o script para digiatar o ceps na calculadora

---
+++ scripts/calculator_script.py
@@ -0,0 +1,43 @@
+from time import sleep
+from traceback import format_exc
+from os import system
+import pyautogui
+import logging
+
+def calculator(ceps:list[dict]) -> None:
+    try:
+        system("calc.exe")
+    except Exception:
+        print("Nâo foi possivel abrir a calculadora")
+
+        logging.error(f"Não foi possivel abrir a calculadora, {format_exc()}")
+
+        return
+
+    sleep(3)
+
+    for cep in ceps:
+        cep = cep["CEP"].replace("-", "")
+
+        try:
+            calculator = pyautogui.getWindowsWithTitle('Calculadora')[0]
+            calculator.activate()
+
+        except Exception:
+            print("A janela da Calculadora não foi encontrada.")
+
+            logging.error(f"A janela da Calculadora não foi encontrada, {format_exc()}")
+
+            return
+        
+        pyautogui.write(cep, interval=0.5)
+
+        sleep(0.5)
+
+        pyautogui.press("esc")
+
+        sleep(0.5)
+
+    print("Script de calculadora correu com sucesso!")
+
+    return
